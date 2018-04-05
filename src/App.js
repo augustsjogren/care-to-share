1e1fc9fadacfe2f136607ed050052be24aa723c6
August Sjögren <augsj305@student.liu.se>
2018-04-05 15:42:10 +0200

    Spotify API playing around

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 import List from './components/List.js';
 import PostCreator from './components/PostCreator.js';
 
-import { Navbar, NavbarBrand, NavbarNav,
+// import Button from 'react-bootstrap';
+
+import { Button, Navbar, NavbarBrand, NavbarNav,
    NavItem, NavLink} from 'mdbreact';
 
 class App extends Component {
@@ -20,6 +22,7 @@ class App extends Component {
                  <NavbarNav className="ml-auto" right>
                    <NavItem active>
                        <NavLink className="nav-link" to="#profile" >Profile</NavLink>
+                       <Button href="http://localhost:3001/api/login"> Sign in </Button>
                    </NavItem>
                  </NavbarNav>
              </Navbar>
--- src/components/PostCreator.js
+++ src/components/PostCreator.js
@@ -13,7 +13,19 @@ import {
 }
   from 'react-bootstrap';
 
-  import { Button} from 'mdbreact';
+import { Button} from 'mdbreact';
+import axios from 'axios';
+
+import SpotifyWebApi from 'spotify-web-api-js';
+
+// var spotifyApi = new SpotifyWebApi();
+//
+// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+//   .then(function(data) {
+//     console.log('Artist albums', data);
+//   }, function(err) {
+//     console.error(err);
+//   });
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -29,7 +41,8 @@ type State = {
   text: string;
   author: atring;
   title: string;
-}
+};
+
 
  class ConnectedPostCreator extends Component<Props, State> {
    constructor(){
@@ -43,7 +56,10 @@ type State = {
      };
 
      this.handleChange = this.handleChange.bind(this);
+     this.handleSearchChange = this.handleSearchChange.bind(this);
      this.handleSubmit = this.handleSubmit.bind(this);
+     this.handleLogin = this.handleLogin.bind(this);
+     this.getHashParams = this.getHashParams.bind(this);
 
    }
 
@@ -67,6 +83,40 @@ type State = {
      });
    }
 
+   handleSearchChange(event) {
+     console.log('change');
+   }
+
+   getHashParams() {
+          var hashParams = {};
+          var e, r = /([^&;=]+)=?([^&;]*)/g,
+              q = window.location.hash.substring(1);
+          while ( e = r.exec(q)) {
+             hashParams[e[1]] = decodeURIComponent(e[2]);
+          }
+          console.log(hashParams);
+          return hashParams;
+        }
+
+
+
+   handleLogin(event){
+
+     event.preventDefault();
+
+     this.getHashParams();
+
+
+
+     // const url = 'http://localhost:3001/api/login';
+     //
+     // axios.get(url)
+     // .then(
+     //   console.log('login')
+     // ).catch(function (error) {
+     //   console.log(error);
+     // });
+   }
 
   render(){
     const {text} = this.state.text;
@@ -86,10 +136,25 @@ type State = {
               placeholder="Write a post"
               onChange={this.handleChange}
             />
+
             <Col xs={5} sm={5} className="buttonCol">
             <Button className="subButton" color="primary" type="submit" block> Submit </Button>
             </Col>
             </FormGroup>
+          </form>
+
+          <form onSubmit={this.handleLogin}>
+            <Row>
+              <Col md={12} >
+              <FormControl
+                placeholder="Search for a track"
+                onChange={this.handleSearchChange}
+              />
+              </Col>
+              <Col md={1} >
+              <Button color="primary" href="http://localhost:3001/api/login" type="submit"> Search  </Button>
+              </Col>
+            </Row>
           </form>
           </Col>
         </Row>
