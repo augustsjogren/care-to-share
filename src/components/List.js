b523d2a5ee81837c8d914d79c236cab4edd99e8b
August Sjögren <augsj305@student.liu.se>
2018-04-17 10:30:23 +0200

    Fixed reverse post order bug

--- src/components/List.js
+++ src/components/List.js
@@ -28,13 +28,15 @@ class ConnectedList extends React.Component<Props, State> {
 
   // Update the state when the component recieves new props (posts)
   componentWillReceiveProps(nextProps){
-    this.setState({data: nextProps.posts.reverse()});
+    this.setState({data: nextProps.posts});
   }
 
   render(){
+    var reversedArray = this.state.data.slice().reverse();
+    
     return(
       <ul className="list-group list-group-flush">
-          {this.state.data.map(el => (
+          {reversedArray.map(el => (
             <Post title={el.title} artist={el.artist} content={el.text} imageUrl={el.imageUrl} author={el.author} key={ el['_id'] }/>
           ))}
         </ul>
